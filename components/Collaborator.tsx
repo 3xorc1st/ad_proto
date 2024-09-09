6fad8d86e7a3d1ebaa088815f35b4e2ccf658165
Alan Rivera <alandavidrcmx@gmail.com>
2024-09-09 13:52:28 -0600

    User_Permissions

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
 import Image from 'next/image';
+import React, { useState } from 'react'
+import UserTypeSelector from './UserTypeSelector';
 import { Button } from './ui/button';
+import { removeCollaborator, updateDocumentAccess } from '@/lib/actions/room.actions';
 
 const Collaborator = ({ roomId, creatorId, collaborator, email, user }: CollaboratorProps) => {
     const [userType, setUserType] = useState(collaborator.userType || 'viewer');
     const [loading, setLoading] = useState(false);
 
-    const shareDocumentHandler = async (type: string) => {}
-    const removeCollaboratorHandler = async (email: string) => {}
+    const shareDocumentHandler = async (type: string) => {
+        setLoading(true);
+
+        await updateDocumentAccess({
+            roomId,
+            email,
+            userType: type as UserType,
+            updatedBy: user
+        });
+
+        setLoading(false);
+    }
+    const removeCollaboratorHandler = async (email: string) => {
+        setLoading(true);
+
+        await removeCollaborator({ roomId, email });
+
+        setLoading(false);
+    }
 
     return (
         <li className="flex items-center justify-between gap-2 py-3">
             <div className="flex gap-2">
-                <Image 
+                <Image
                     src={collaborator.avatar}
                     alt={collaborator.name}
                     width={36}
@@ -34,12 +53,12 @@ const Collaborator = ({ roomId, creatorId, collaborator, email, user }: Collabor
 
             {creatorId === collaborator.id ? (
                 <p className="text-sm text-blue-100">Owner</p>
-            ): (
+            ) : (
                 <div className="flex items-center">
-                    <UserTypeSelector 
+                    <UserTypeSelector
                         userType={userType as UserType}
                         setUserType={setUserType || 'viewer'}
-                        onClickHandler={shareDocumentHandler} 
+                        onClickHandler={shareDocumentHandler}
                     />
                     <Button type="button" onClick={() => removeCollaboratorHandler(collaborator.email)}>
                         Remove
