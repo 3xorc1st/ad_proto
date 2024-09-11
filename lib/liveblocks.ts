c60559022e96fd264033ddccdfa26600a127c519
Alan Rivera <alandavidrcmx@gmail.com>
2024-09-11 09:09:53 -0600

    last tutorial commit

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -3,5 +3,3 @@ import { Liveblocks } from "@liveblocks/node";
 export const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
 });
-
-console.log("Liveblocks Secret Key:", process.env.LIVEBLOCKS_SECRET_KEY);
