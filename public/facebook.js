02fbaa2802ae6fdf6ca06ad218f3d45922ec769d
Alan Rivera <alandavidrcmx@gmail.com>
2024-11-25 11:26:59 -0600

    Facebook 1

---
+++ public/facebook.js
@@ -0,0 +1,18 @@
+// public/facebook.js
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1056524272882700', // Replace with your Facebook app id
+        cookie: true,
+        xfbml: true,
+        version: 'v16.0'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
